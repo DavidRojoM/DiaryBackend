ff9ccba19ab31480000499617748532c4af28faf
David Rojo <davidrojo1997@gmail.com>
2021-09-01 02:58:15 +0200

    User model, routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
+const cors = require('cors')
 require('dotenv').config()
+
 const app = express()
 require('./database')
 
-app.listen(process.env.PORT)
+app.use(cors())
+app.use('/api', require('./routes/index'))
 
+app.listen(process.env.PORT)
 console.log('server on port', 3000)
---
+++ src/models/user.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require('mongoose')
+const userSchema = new Schema({
+  email: String,
+  password: String
+})
+module.exports = model('User', userSchema)
