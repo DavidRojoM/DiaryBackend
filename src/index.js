d4df51eb360b342842eec53713a332fc72f82fd9
David Rojo <davidrojo1997@gmail.com>
2021-09-04 03:26:50 +0200

    Sign In, Sign Up tokens, morgan middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 require('dotenv').config()
 
 const app = express()
 require('./database')
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.use(cors())
+app.use(morgan('dev'))
 app.use('/api', require('./routes/index'))
 
-app.listen(process.env.PORT)
-console.log('server on port', 3000)
+app.set('PORT', process.env.PORT || 3000)
+
+app.listen(app.get('PORT'))
+console.log('server on port', app.get('PORT'))
